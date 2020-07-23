98cc942c2c8320ba43d49b5976881b3079d2a983
Harshit Shah <harshit@Harshit.local>
2020-07-23 13:32:47 +0530

    changed to class component

--- src/Components/Shared/ShoppingCard.js
+++ src/Components/Shared/ShoppingCard.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React from 'react';
+import {withStyles} from '@material-ui/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -10,7 +10,7 @@ import Typography from '@material-ui/core/Typography';
 import Faker from 'faker';
 import {Grid} from '@material-ui/core';
 
-const useStyles = makeStyles({
+const styles = (theme) => ({
   root: {
     maxWidth: 345,
   },
@@ -20,69 +20,78 @@ const useStyles = makeStyles({
   title: {},
 });
 
-export default function MediaCard(props) {
-  const {data} = props;
-  const classes = useStyles();
-  const [count, setcount] = useState(0);
-  const onCartClick = () => {
-    setcount(count + 1);
+class ShoppingCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  onCartClick = () => {
+    console.log('data', this.props.data);
+    // cartArray.push(data);
+    // setcount(count + 1);
   };
-  console.log('count', count);
-  console.log('data', data);
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={Faker.image.food()}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography
-            gutterBottom
-            component="data"
-            className={classes.title}
-            variant="body1">
-            {data.item}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="p"
-            gutterBottom>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500
-          </Typography>
-          <Grid container justify="space-between">
+  render() {
+    const {classes} = this.props;
+    console.log('rposp', this.props);
+    return (
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={Faker.image.food()}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
             <Typography
               gutterBottom
               component="data"
               className={classes.title}
               variant="body1">
-              Amount
+              {this.props.data.item}
             </Typography>
-            <Grid item>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              gutterBottom>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500
+            </Typography>
+            <Grid container justify="space-between">
               <Typography
                 gutterBottom
                 component="data"
                 className={classes.title}
                 variant="body1">
-                {data.price}
+                Amount
               </Typography>
+              <Grid item>
+                <Typography
+                  gutterBottom
+                  component="data"
+                  className={classes.title}
+                  variant="body1">
+                  {this.props.data.price}
+                </Typography>
+              </Grid>
             </Grid>
-          </Grid>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" onClick={onCartClick}>
-          Add to cart
-        </Button>
-        {count}
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  );
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary" onClick={this.onCartClick}>
+            Add to cart
+          </Button>
+          {this.state.count}
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
 }
+
+export default withStyles(styles)(ShoppingCard);
